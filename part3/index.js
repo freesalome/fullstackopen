50b3b88be63cb3fe072e86dcbdc4411dbc9922df
User <181531679+freesalome@users.noreply.github.com>
2026-02-04 19:30:27 -0500

    uibuild

--- part3/index.js
+++ part3/index.js
@@ -77,6 +77,6 @@ app.post('/api/persons', (req, res) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
